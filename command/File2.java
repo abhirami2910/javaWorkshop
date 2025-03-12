bef9bacd26b948d1253297122945f02a7b9f7e7a
abhirami2910 <abhirami29102005@gmail.com>
2025-03-12 14:17:40 +0530

    this is completed

---
+++ command/File2.java
@@ -0,0 +1,9 @@
+public class File2{
+    /*
+        1)demo file
+    */
+    public static void main(String args[]){
+        int num=10;//this is int value
+        System.out.println(num);
+    }
+}
