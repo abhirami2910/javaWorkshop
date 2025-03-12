f430769ef025703625871d61d1fd58c1b054de03
abhirami2910 <abhirami29102005@gmail.com>
2025-03-12 14:33:37 +0530

    this program completed

---
+++ Method/File1.java
@@ -0,0 +1,10 @@
+public class File1{
+    public void add(int num1,int num2){
+        int num=num1+num2;
+        System.out.println(num);
+    }
+    public static void main(String args[]){
+        File1 obj=new File1();
+        obj.add(10,20);
+    }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
