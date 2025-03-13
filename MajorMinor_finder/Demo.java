1272f1a520a31cc10d6919bddf85bf557d36a9ae
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 11:08:50 +0530

    finder program

---
+++ MajorMinor_finder/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    public static void main(String args[]){
+        int Age=32;
+        if(Age>=18){
+            System.out.println("they are major");
+        }
+        else{
+            System.out.println("they are minor");
+        }
+    }
+}
