6edf09f122a08daf53e98ba321fa3cc06b2debd1
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 15:06:32 +0530

    namr is printed

---
+++ Scanner/File.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your Name");
+        String name=input.nextLine();
+        System.out.println(name);
+    }
+}
