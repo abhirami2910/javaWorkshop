ecb85d92f73cf78735e2246887808c28a5a0a789
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 15:20:44 +0530

    Addition is completed

---
+++ ScannerAdd/File.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("num1:");
+        int a=sc.nextInt();
+        System.out.println("num2:");
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("total:"+c);
+    }
+}
