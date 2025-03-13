86db7df553c4108d85c185a9970e9e7a1e222b7e
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 10:59:02 +0530

    my program completed

---
+++ if_else/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    public static void main(String args[]){
+        if(false){
+            System.out.println("condition is true");
+        }
+        else
+        {
+            System.out.println("condition is false");
+        }
+    }
+}
