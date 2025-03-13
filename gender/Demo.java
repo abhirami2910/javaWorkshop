749474e51ee62b7111fb36cdffedfa6785c34669
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 11:33:40 +0530

    gender is completed

---
+++ gender/Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+    public static void main(String args[]){
+        String gender="M";
+        if(gender=="M"){
+            System.out.println("condition is true");
+        }
+        else
+        {
+            System.out.println("condition is false");
+        }
+    }
+}
