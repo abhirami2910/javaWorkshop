50aa731e65e714ff255e00d42403fce05908e941
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 11:13:08 +0530

    number is finded

---
+++ positive_negative/Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    public static void main(String args[]){
+        int num=32;
+        if(num>0){
+            System.out.println("Number is positive");
+        }
+        else{
+            System.out.println("Number is negative");
+        }
+    }
+}
