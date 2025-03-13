2dd0d290ecbd761e2d7ceec9816d30dbf331836d
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 14:23:13 +0530

    leap year found

---
+++ LeapYear/File.java
@@ -0,0 +1,13 @@
+public class File{
+    public static void main(String args[]){
+        int year=2020;
+        if(year%4==0){
+        System.out.println("leap year");
+        }
+        else
+    {
+        System.out.println("not leap year");
+    }
+        
+    }
+}
