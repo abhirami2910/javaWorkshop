32d4cddcb2cc40903381ca53016907ed0ee7563b
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 15:10:48 +0530

    nextint age is completed

---
+++ NextInt/File.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age=input.nextInt();
+        System.out.println(age);
+    }
+}
