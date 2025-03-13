28d1db22f3b1a28d39821a1659689f135359f2bf
abhirami2910 <abhirami29102005@gmail.com>
2025-03-13 10:22:21 +0530

    task is completed

---
+++ ArithmeticOperator/File.java
@@ -0,0 +1,9 @@
+public class File{
+    public static void main(String args[]){
+        System.out.println(12+3);
+        System.out.println(12-3);
+        System.out.println(12*3);
+        System.out.println(12/3);
+        System.out.println(12%3);
+    }
+}
