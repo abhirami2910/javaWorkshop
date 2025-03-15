94d205f180c391920f77cfbe84e033da5a18fd3e
abhirami2910 <abhirami29102005@gmail.com>
2025-03-15 10:40:39 +0530

    constructor program executed

---
+++ Constructor/Demo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Demo{
+    Demo(){
+        System.out.println("Hello");
+
+    }
+    public static void main(String args[]){
+        Demo obj=new Demo();
+    }
+}
