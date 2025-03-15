6878fdbe38af11a95d9518b643d638c580c8737f
abhirami2910 <abhirami29102005@gmail.com>
2025-03-15 10:10:36 +0530

    cases are completed

---
+++ Cases/Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+    public static void main(String args[]){
+        String x="Hello programmer";
+        System.out.println(x.toUpperCase());
+        System.out.println(x.toLowerCase());
+    }
+}
